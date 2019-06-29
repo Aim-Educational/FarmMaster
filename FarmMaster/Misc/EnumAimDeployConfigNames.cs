9e48550a4be7e6cbab9ff19513f13dde1a2f9efd
SealabJaster <sealabjaster@gmail.com>
2019-06-29 22:10:26 +0100

    Initial commit

---
+++ FarmMaster/Misc/EnumAimDeployConfigNames.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmMaster.Misc
+{
+    public static class EnumAimDeployConfigNames
+    {
+        public const string ConnectionString_Migrate = "ConnectionStrings:Migrate";
+        public const string ConnectionString_General = "ConnectionStrings:General";
+        public const string Smtp_Host                = "Smtp:Host";
+        public const string Smtp_Port                = "Smtp:Port";
+        public const string Smtp_Username            = "Smtp:Username";
+        public const string Smtp_Password            = "Smtp:Password";
+    }
+}
--- FarmMaster/Startup.cs
+++ FarmMaster/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -20,18 +21,25 @@ namespace FarmMaster
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            // Cookie consent.
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.MinimumSameSitePolicy = SameSiteMode.Strict;
             });
 
+            // HSTS
+            services.AddHsts(o => 
+            {
+                o.Preload = true;
+                o.IncludeSubDomains = true;
+                o.MaxAge = TimeSpan.FromDays(365);
+            });
 
+            // MVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -41,17 +49,26 @@ namespace FarmMaster
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
+            // For nginx
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
             app.UseHttpsRedirection();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
