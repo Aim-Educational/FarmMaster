8c7030d9e28b98ce33ac384c15735e86b2eb9719
SealabJaster <sealabjaster@gmail.com>
2019-06-29 21:31:58 +0100

    Add project files.

---
+++ FarmMaster/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FarmMaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
