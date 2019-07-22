55d65fabbf643db6dbef45ed82cd671f999478b7
SealabJaster <sealabjaster@gmail.com>
2019-07-22 21:00:30 +0100

    Make ComponentTable more modular

--- FarmMaster/Models/ComponentTableViewModels.cs
+++ FarmMaster/Models/ComponentTableViewModels.cs
@@ -10,12 +10,12 @@ namespace FarmMaster.Models
 
     }
 
-    public class ComponentTableTextNameTextValueInput : ComponentTableInputBase
+    public class ComponentTableTextInput : ComponentTableInputBase
     {
 
     }
 
-    public class ComponentTableTextNameSelectValueInput : ComponentTableInputBase
+    public class ComponentTableSelectInput : ComponentTableInputBase
     {
         public Dictionary<string, string> SelectValues { get; set; }
     }
@@ -32,9 +32,17 @@ namespace FarmMaster.Models
         public string                               Name        { get; set; }
         public string                               Header      { get; set; }
         public IEnumerable<ComponentTableNameValue> NameValues  { get; set; }
-        public ComponentTableInputBase              Input       { get; set; }
+        public ComponentTableInputBase              NameInput   { get; set; }
+        public ComponentTableInputBase              ValueInput  { get; set; }
         public string                               AjaxAdd     { get; set; }
         public string                               AjaxRemove  { get; set; }
         public int?                                 DataId      { get; set; }
+    }
+
+    public class ComponentTableInputPartialViewModel
+    {
+        public string InputHeader { get; set; }
+        public ComponentTableInputBase Input { get; set; }
+        public string IdInput { get; set; }
     }
 }
