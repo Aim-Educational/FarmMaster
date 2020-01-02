02965017c12d64bc19e0cce886efa5bb71c4326a
SealabJaster <sealabjaster@gmail.com>
2020-01-02 01:21:22 +0000

    Create migration to reflect permission changes

--- Business/Migrations/20200102012111_PermissionTweaks.Designer.cs
+++ Business/Migrations/20200102012111_PermissionTweaks.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Business.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Business.Migrations
 {
     [DbContext(typeof(FarmMasterContext))]
-    partial class FarmMasterContextModelSnapshot : ModelSnapshot
+    [Migration("20200102012111_PermissionTweaks")]
+    partial class PermissionTweaks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
