9479f316da4fa2e68148e304c7b5881241f3d09f
SealabJaster <sealabjaster@gmail.com>
2020-01-14 06:32:21 +0000

    Misc interactivity, and removing an unused file.

---
+++ FarmMaster/Models/AnimalGroupScriptExecutionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmMaster.Models
+{
+    public class AnimalGroupScriptExecutionViewModel
+    {
+        public int GroupId { get; set; }
+        public string AnimalGridId { get; set; }
+    }
+}
