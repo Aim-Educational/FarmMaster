651339e62bdbfe2be19e05d02fcd610ebee16e0a
SealabJaster <sealabjaster@gmail.com>
2020-01-16 10:21:28 +0000

    Progress towards making the group script editor partial more featureful

---
+++ FarmMaster/Controllers/GroupScriptController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmMaster.Controllers
+{
+    public class GroupScriptController : Controller
+    {
+        public IActionResult Editor()
+        {
+            return View();
+        }
+    }
+}
--- FarmMaster/Models/AnimalGroupScriptExecutionViewModel.cs
+++ FarmMaster/Models/AnimalGroupScriptExecutionViewModel.cs
@@ -5,7 +5,11 @@ using System.Threading.Tasks;
 
 namespace FarmMaster.Models
 {
-    public class AnimalGroupScriptExecutionViewModel
+    public abstract class AnimalGroupScriptEditorViewModel
+    {
+    }
+
+    public class AnimalGroupScriptEditorSingleUseViewModel :  AnimalGroupScriptEditorViewModel
     {
         public int GroupId { get; set; }
         public string AnimalGridId { get; set; }
