04d24caec8f1f0386b39457d709ee783c5f73a8f
SealabJaster <sealabjaster@gmail.com>
2020-01-31 03:29:53 +0000

    Add a migration containing the SQL side of all the new GroupScript changes

--- Business/Migrations/20200131032611_GroupScript_NOT_and_BREEDIS.Designer.cs
+++ Business/Migrations/20200131032611_GroupScript_NOT_and_BREEDIS.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Business.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Business.Migrations
 {
     [DbContext(typeof(FarmMasterContext))]
-    partial class FarmMasterContextModelSnapshot : ModelSnapshot
+    [Migration("20200131032611_GroupScript_NOT_and_BREEDIS")]
+    partial class GroupScript_NOT_and_BREEDIS
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
