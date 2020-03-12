05116aa36ea96781a4ebfa393f1956c5237ff0be
SealabJaster <sealabjaster@gmail.com>
2020-03-12 06:51:04 +0000

    Start of rewrite. Initial start to custom identity context

---
+++ FarmMaster/DataAccess/Identity.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    class ApplicationUser : IdentityUser<int>
+    {
+    }
+
+    class ApplicationUserClaim : IdentityUserClaim<int>
+    {
+    }
+
+    class ApplicationUserLogin : IdentityUserLogin<int>
+    {
+    }
+
+    class ApplicationRole : IdentityRole<int>
+    { 
+    }
+
+    class ApplicationRoleClaim : IdentityRoleClaim<int>
+    {
+    }
+
+    class ApplicationUserRole : IdentityUserRole<int>
+    { 
+    }
+
+    class ApplicationUserToken : IdentityUserToken<int>
+    {
+    }
+
+    class IdentityContext : IdentityDbContext<
+        ApplicationUser, 
+        ApplicationRole, 
+        int, 
+        ApplicationUserClaim, 
+        ApplicationUserRole,
+        ApplicationUserLogin, 
+        ApplicationRoleClaim, 
+        ApplicationUserToken
+    >
+    {
+
+    }
+
+    class IdentityContextFactory : IDesignTimeDbContextFactory<IdentityContext>
+    {
+        public IdentityContext CreateDbContext(string[] args)
+        {
+            throw new NotImplementedException("TODO:");
+        }
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Documentation & API Reference
-
-Found here: https://aim-educational.github.io/FarmMaster/ (Click the links at the top)
