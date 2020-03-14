75559a520968699c020132ed0d8171820617f6af
SealabJaster <sealabjaster@gmail.com>
2020-03-14 07:09:57 +0000

    Add dynamic item menu

---
+++ FarmMaster/Constants/RoleNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmMaster.Constants
+{
+    public static class RoleNames
+    {
+        public const string SUPER_ADMIN = "super_admin";
+    }
+}
