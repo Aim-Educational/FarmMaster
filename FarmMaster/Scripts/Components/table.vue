3fe6cbf684808515ab026dfa94d821e33012d499
SealabJaster <sealabjaster@gmail.com>
2020-03-15 09:05:39 +0000

    Make the table component more consistent

--- FarmMaster/Scripts/Components/table.vue
+++ FarmMaster/Scripts/Components/table.vue
@@ -68,7 +68,7 @@ const ROWT_EXAMPLE = {
  *   'selected': {
  *      Emitted whenever a checkbox is checked. Payload is the following object.
  *      
- *      triggerValue:   Object   # The value from `props.values` that was selected. THIS IS NULL if the 'selectAll' checkbox was checked.
+ *      triggerValue:   Object   # The value from `props.values` that was selected. THIS IS NULL if the 'selectAll' checkbox was checked. THIS IS NULL if the trigger was caused a column being sorted
  *      selectedValues: Object[] # All values from `props.values` that are currently selected, including the `triggerValue`.
  *   }
  */
@@ -172,9 +172,13 @@ export default {
                 this.sortedRow.row = null;
 
             // If selectAll is already false, then the watcher won't trigger, so we need to deselect stuff anyway just in case.
-            this.selectAll = false;
             for(let i = 0; i < this.selectedValueIndicies.length; i++)
                 this.selectedValueIndicies[i] = false;
+
+            this.ignoreNextSelectAll = true;
+            this.selectAll = false;
+
+            this.$emit("selected", { triggerValue: null, selectedValues: [] });
         },
 
         onValueChecked(index) {
