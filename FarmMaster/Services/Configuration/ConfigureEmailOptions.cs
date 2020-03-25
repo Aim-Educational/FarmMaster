5e339322faab9fdad5bbab68a2886230fbfb7a19
SealabJaster <sealabjaster@gmail.com>
2020-03-25 23:08:19 +0000

    Add a basic email layout

--- FarmMaster/Services/Configuration/ConfigureEmailOptions.cs
+++ FarmMaster/Services/Configuration/ConfigureEmailOptions.cs
@@ -1,10 +1,12 @@
 ﻿using DataAccess;
 using EmailSender;
 using FarmMaster.Services;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,8 +14,13 @@ namespace FarmMaster.Services.Configuration
 {
     public class ConfigureEmailOptions : ConfigWithDatabaseService<EmailSenderConfig>
     {
-        public ConfigureEmailOptions(IServiceScopeFactory factory) : base(factory)
-        {}
+        readonly string _layoutTemplate;
+
+        public ConfigureEmailOptions(IServiceScopeFactory factory, IWebHostEnvironment env) : base(factory)
+        {
+            var path = Path.Combine(env.WebRootPath, "email", "layout.html");
+            this._layoutTemplate = File.ReadAllText(path);
+        }
 
         protected override void ConfigWithDatabase(EmailSenderConfig options, FarmMasterContext db)
         {
@@ -22,7 +29,7 @@ namespace FarmMaster.Services.Configuration
                 return;
 
             options.Url = "localhost"; // TODO: Don't hardcode
-            options.Layout = new EmailTemplate("{{ body }}"); // TODO: Load from file
+            options.Layout = new EmailTemplate(this._layoutTemplate);
 
             options.Smtp.Server   = settings.SmtpServer;
             options.Smtp.Port     = settings.SmtpPort;
