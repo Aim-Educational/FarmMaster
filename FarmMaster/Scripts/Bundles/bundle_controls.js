01e91a06903503cb0e956ec44394990f8a23097e
SealabJaster <sealabjaster@gmail.com>
2020-03-25 16:36:26 +0000

    Add confirm-button

--- FarmMaster/Scripts/Bundles/bundle_controls.js
+++ FarmMaster/Scripts/Bundles/bundle_controls.js
@@ -3,9 +3,11 @@ import Textbox from "../Components/textbox.vue";
 import Alert from "../Components/alert.vue";
 import FarmValueTable from "../Components/table_values.vue";
 import PermEditor from "../Components/editor_permissions.vue";
+import ConfirmButton from "../Components/confirm_button.vue";
 
 Vue.component("window", Window);
 Vue.component("textbox", Textbox);
 Vue.component("alert", Alert);
 Vue.component("fm-value-table", FarmValueTable);
 Vue.component("editor-perms", PermEditor);
+Vue.component("confirm-button", ConfirmButton);
---
+++ FarmMaster/Scripts/Components/confirm_button.vue
@@ -0,0 +1,44 @@
+<template>
+    <button @click="onClick"
+            @mouseout="onMouseOut">
+            {{ buttonText }}
+        </button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: "",
+            showConfirm: false
+        }
+    },
+
+    props: {
+        prompt: String,
+        confirm: String
+    },
+
+    computed: {
+        buttonText() {
+            return this.showConfirm ? this.confirm : this.prompt;
+        }
+    },
+
+    methods: {
+        onClick(e) {
+            if(!this.showConfirm) {
+                this.showConfirm = true;
+                return;
+            }
+
+            this.showConfirm = false;
+            this.$emit("click", e);
+        },
+
+        onMouseOut() {
+            this.showConfirm = false;
+        }
+    }
+}
+</script>
