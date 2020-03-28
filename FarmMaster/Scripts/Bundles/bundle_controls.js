61a968f7bddcb9e9aef16b9b889d2003f7cf6fe6
SealabJaster <sealabjaster@gmail.com>
2020-03-28 22:18:25 +0000

    Add ToggleButton

--- FarmMaster/Scripts/Bundles/bundle_controls.js
+++ FarmMaster/Scripts/Bundles/bundle_controls.js
@@ -4,6 +4,7 @@ import Alert from "../Components/alert.vue";
 import FarmValueTable from "../Components/table_values.vue";
 import PermEditor from "../Components/editor_permissions.vue";
 import ConfirmButton from "../Components/confirm_button.vue";
+import ToggleButton from "../Components/toggle_button.vue";
 
 Vue.component("window", Window);
 Vue.component("textbox", Textbox);
@@ -11,3 +12,4 @@ Vue.component("alert", Alert);
 Vue.component("fm-value-table", FarmValueTable);
 Vue.component("editor-perms", PermEditor);
 Vue.component("confirm-button", ConfirmButton);
+Vue.component("toggle-button", ToggleButton);
---
+++ FarmMaster/Scripts/Components/toggle_button.vue
@@ -0,0 +1,38 @@
+<template>
+    <button @click="onClick">
+        {{ buttonText }}
+    </button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            toggled: false
+        }
+    },
+
+    props: {
+        untoggledText: String,
+        toggledText: String,
+        value: Boolean
+    },
+
+    computed: {
+        buttonText() {
+            return this.toggled ? this.toggledText : this.untoggledText;
+        }
+    },
+
+    methods: {
+        onClick(e) {
+            this.toggled = !this.toggled;
+            this.$emit("input", this.toggled);
+        }
+    },
+
+    created() {
+        this.toggled = this.value;
+    }
+}
+</script>
