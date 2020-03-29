6592df0e388fbb17432681fa05c7d1a2a4634c22
SealabJaster <sealabjaster@gmail.com>
2020-03-29 18:54:40 +0100

    Add modal vue component

--- FarmMaster/Scripts/Bundles/bundle_controls.js
+++ FarmMaster/Scripts/Bundles/bundle_controls.js
@@ -5,6 +5,7 @@ import FarmValueTable from "../Components/table_values.vue";
 import PermEditor from "../Components/editor_permissions.vue";
 import ConfirmButton from "../Components/confirm_button.vue";
 import ToggleButton from "../Components/toggle_button.vue";
+import Modal from "../Components/modal.vue";
 
 Vue.component("window", Window);
 Vue.component("textbox", Textbox);
@@ -13,3 +14,4 @@ Vue.component("fm-value-table", FarmValueTable);
 Vue.component("editor-perms", PermEditor);
 Vue.component("confirm-button", ConfirmButton);
 Vue.component("toggle-button", ToggleButton);
+Vue.component("modal", Modal);
---
+++ FarmMaster/Scripts/Components/modal.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="modal-background" 
+         :class="{ 'hidden': !isShowing }"
+         @click="hide()">
+        <div class="modal"
+             @click.stop>
+            <div class="header">
+                <slot name="header">
+                </slot>
+            </div>
+            <div class="body">
+                <slot name="body">
+                </slot>
+            </div>
+            <div class="footer">
+                <slot name="footer">
+                </slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isShowing: false
+        };
+    },
+
+    methods: {
+        show() {
+            this.isShowing = true;
+        },
+
+        hide() {
+            this.isShowing = false;
+        }
+    },
+
+    created() {
+        document.addEventListener("keydown", e => {
+            if(e.key === "Escape" && this.isShowing)
+                this.hide();
+        });
+    }
+}
+</script>
