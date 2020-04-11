a457a0812e4f0c657ab640d63ce69c665e67bbc0
SealabJaster <sealabjaster@gmail.com>
2020-04-11 03:06:15 +0100

    Modify AddDataAccessGraphQLSchema such that it can auto detect and register the RootResolver services

--- DataAccessGraphQL/DataAccessGraphQLExtensions.cs
+++ DataAccessGraphQL/DataAccessGraphQLExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccessGraphQL
@@ -10,11 +11,29 @@ namespace DataAccessGraphQL
     {
         public static IServiceCollection AddDataAccessGraphQLSchema(this IServiceCollection services)
         {
-            return services.AddScoped<UserRootResolver>()
-                           .AddScoped<ContactRootResolver>()
-                           .AddScoped<SpeciesRootResolver>()
+            return services.AddRootResolvers()
                            .AddScoped<DataAccessGraphQLSchema>()
                            .AddScoped<GraphQLUserContextAccessor>();
+        }
+
+        static IServiceCollection AddRootResolvers(this IServiceCollection services)
+        {
+            // Get the DataAccessGraphQL assembly, as we're gonna inspect all classes to automatically
+            // register the root resolvers.
+            var thisAssembly = typeof(DataAccessGraphQLExtensions).Assembly;
+
+            //    Get all types in DataAccessGraphQL
+            // -> Find all classes that end in "RootResolver", except the actual RootResolver base class itself.
+            var resolvers = thisAssembly
+                            .DefinedTypes
+                            .Where(t => t.IsClass)
+                            .Where(t => t.Name.EndsWith("RootResolver"))
+                            .Where(t => t != typeof(RootResolver<>)); // Don't want the actual base class included.
+            
+            foreach(var resolver in resolvers)
+                services.AddScoped(resolver);
+
+            return services;
         }
     }
 }
