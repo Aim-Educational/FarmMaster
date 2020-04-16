2c56abaae49ae9dc2e6aa8839c80c168cfa86487
SealabJaster <sealabjaster@gmail.com>
2020-04-16 08:41:04 +0100

    Add Seeding for Species and Breeds

--- DataAccess/FarmMasterContext.cs
+++ DataAccess/FarmMasterContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccess
@@ -44,6 +45,8 @@ namespace DataAccess
              .HasOne(br => br.NoteOwner)
              .WithMany()
              .OnDelete(DeleteBehavior.Cascade);
+
+            this.Seed(b);
         }
 
         #region Tables
@@ -54,6 +57,76 @@ namespace DataAccess
         public DbSet<NoteEntry> NoteEntries { get; set; }
         public DbSet<Species>   Species     { get; set; }
         public DbSet<Breed>     Breeds      { get; set; }
+        #endregion
+
+        // Identity uses a runtime call to Seed because I need to make use of the UserManager etc. services to ensure
+        // I don't mess things up.
+        //
+        // FarmMasterContext however will use the usual EF migration seeding, as we don't need to rely on services for this.
+        #region Seeding
+        void Seed(ModelBuilder b)
+        {
+            this.SeedSpeciesBreeds(b);
+        }
+
+        void SeedSpeciesBreeds(ModelBuilder b)
+        {
+            // So we don't have a chance in hell to overwrite user-made species/breeds.
+            const int BASE_ID = int.MaxValue - 100_000;
+
+            // Id will be added onto BASE_ID
+            var speciesList = new List
+            <
+                (int id, string name, int gestrationDays, (int id, string name)[] breeds)
+            >()
+            {
+                (1, "Cow", 283, new[]
+                { 
+                    (1,  "English Longhorn"),
+                    (2,  "Red Poll"),
+                    (3,  "White Park"),
+                    (4,  "Hereford"),
+                    (5,  "Highland"),
+                    (6,  "Aryshire"),
+                    (7,  "Aberdeen Angus"),
+                    (8,  "South Devon"),
+                    (9,  "British White"),
+                    (10, "Belted Galloway")
+                })
+            };
+
+            // Ensure we don't have duplicate ids
+            var speciesIndexSet = new HashSet<int>();
+            var breedIndexSet   = new HashSet<int>();
+            foreach(var species in speciesList)
+            {
+                foreach(var breed in species.breeds)
+                {
+                    if(!breedIndexSet.Add(breed.id))
+                        throw new InvalidOperationException($"Breed {breed.name} of Species {species.name} has duplicate ID of {breed.id}");
+                }
+
+                if(!speciesIndexSet.Add(species.id))
+                    throw new InvalidOperationException($"Species {species.name} has duplicate ID of {species.id}");
+            }
+
+            // Seed all the data.
+            b.Entity<Species>()
+            .HasData(
+                speciesList
+                .Select(s => new Species { SpeciesId = s.id, Name = s.name, GestrationPeriod = TimeSpan.FromDays(s.gestrationDays) })
+            );
+
+            b.Entity<Breed>()
+            .HasData(
+                speciesList
+                .SelectMany(s => 
+                    s
+                    .breeds
+                    .Select(br => new Breed { BreedId = br.id, SpeciesId = s.id, Name = br.name })
+                )
+            );
+        }
         #endregion
     }
 
---
+++ DataAccess/Migrations/20200416073857_SpeciesBreedSeed.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class SpeciesBreedSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Species",
+                columns: new[] { "SpeciesId", "GestrationPeriod", "Name", "NoteOwnerId" },
+                values: new object[] { 1, new TimeSpan(283, 0, 0, 0, 0), "Cow", null });
+
+            migrationBuilder.InsertData(
+                table: "Breeds",
+                columns: new[] { "BreedId", "Name", "NoteOwnerId", "SpeciesId" },
+                values: new object[,]
+                {
+                    { 1, "English Longhorn", null, 1 },
+                    { 2, "Red Poll", null, 1 },
+                    { 3, "White Park", null, 1 },
+                    { 4, "Hereford", null, 1 },
+                    { 5, "Highland", null, 1 },
+                    { 6, "Aryshire", null, 1 },
+                    { 7, "Aberdeen Angus", null, 1 },
+                    { 8, "South Devon", null, 1 },
+                    { 9, "British White", null, 1 },
+                    { 10, "Belted Galloway", null, 1 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Breeds",
+                keyColumn: "BreedId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Breeds",
+                keyColumn: "BreedId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Breeds",
+                keyColumn: "BreedId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Breeds",
+                keyColumn: "BreedId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Breeds",
+                keyColumn: "BreedId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Breeds",
+                keyColumn: "BreedId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Breeds",
+                keyColumn: "BreedId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Breeds",
+                keyColumn: "BreedId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Breeds",
+                keyColumn: "BreedId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Breeds",
+                keyColumn: "BreedId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Species",
+                keyColumn: "SpeciesId",
+                keyValue: 1);
+        }
+    }
+}
