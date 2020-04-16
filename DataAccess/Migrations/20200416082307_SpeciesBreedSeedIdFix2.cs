4aadf72b7e1caabaa76dd00b2e8972782ba37841
SealabJaster <sealabjaster@gmail.com>
2020-04-16 09:31:53 +0100

    Clean up migrations

--- DataAccess/Migrations/20200416082307_SpeciesBreedSeedIdFix2.cs
+++ DataAccess/Migrations/20200416082307_SpeciesBreedSeedIdFix2.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 namespace DataAccess.Migrations
 {
@@ -6,55 +7,10 @@ namespace DataAccess.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DeleteData(
-                table: "Breeds",
-                keyColumn: "BreedId",
-                keyValue: 2);
-
-            migrationBuilder.DeleteData(
-                table: "Breeds",
-                keyColumn: "BreedId",
-                keyValue: 3);
-
-            migrationBuilder.DeleteData(
-                table: "Breeds",
-                keyColumn: "BreedId",
-                keyValue: 4);
-
-            migrationBuilder.DeleteData(
-                table: "Breeds",
-                keyColumn: "BreedId",
-                keyValue: 5);
-
-            migrationBuilder.DeleteData(
-                table: "Breeds",
-                keyColumn: "BreedId",
-                keyValue: 6);
-
-            migrationBuilder.DeleteData(
-                table: "Breeds",
-                keyColumn: "BreedId",
-                keyValue: 7);
-
-            migrationBuilder.DeleteData(
-                table: "Breeds",
-                keyColumn: "BreedId",
-                keyValue: 8);
-
-            migrationBuilder.DeleteData(
-                table: "Breeds",
-                keyColumn: "BreedId",
-                keyValue: 9);
-
-            migrationBuilder.DeleteData(
-                table: "Breeds",
-                keyColumn: "BreedId",
-                keyValue: 10);
-
-            migrationBuilder.DeleteData(
-                table: "Breeds",
-                keyColumn: "BreedId",
-                keyValue: 11);
+            migrationBuilder.InsertData(
+                table: "Species",
+                columns: new[] { "SpeciesId", "GestrationPeriod", "Name", "NoteOwnerId" },
+                values: new object[] { 1, new TimeSpan(283, 0, 0, 0, 0), "Cow", null });
 
             migrationBuilder.InsertData(
                 table: "Breeds",
@@ -125,23 +81,6 @@ namespace DataAccess.Migrations
                 table: "Breeds",
                 keyColumn: "BreedId",
                 keyValue: 2147383657);
-
-            migrationBuilder.InsertData(
-                table: "Breeds",
-                columns: new[] { "BreedId", "Name", "NoteOwnerId", "SpeciesId" },
-                values: new object[,]
-                {
-                    { 2, "English Longhorn", null, 1 },
-                    { 3, "Red Poll", null, 1 },
-                    { 4, "White Park", null, 1 },
-                    { 5, "Hereford", null, 1 },
-                    { 6, "Highland", null, 1 },
-                    { 7, "Aryshire", null, 1 },
-                    { 8, "Aberdeen Angus", null, 1 },
-                    { 9, "South Devon", null, 1 },
-                    { 10, "British White", null, 1 },
-                    { 11, "Belted Galloway", null, 1 }
-                });
         }
     }
 }
