4ef5a414dfdd4b7a8acac058c0dd68033ea07422
SealabJaster <sealabjaster@gmail.com>
2020-04-17 09:39:58 +0100

    comment

--- FarmMaster/Scripts/Components/_common.ts
+++ FarmMaster/Scripts/Components/_common.ts
@@ -1,5 +1,12 @@
+/**
+ * Exports certain values/functions that are shared between different components.
+ * 
+ * NOTE: Only things that are exclusive to Vue and Vue components should be placed here, anything else should be made into a library.
+ */
 export default {
+    /// Common props that a vue component may have.
     vueProps: {
+        /// Usually exposed as "mode", this is used by components that can be toggled between a "view" or "edit" mode.
         viewOrEdit: {
             type: String,
             default: "edit",
