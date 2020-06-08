434ecd661805202deafceda0a6b2ef3738f1f3cc
SealabJaster <sealabjaster@gmail.com>
2020-06-08 06:04:16 +0100

    Improve CrudPartial. Add completed CRUD test for Contact (excl. Notes)

--- FarmMasterTests/Common/FarmClientPartials/CrudPartial.cs
+++ FarmMasterTests/Common/FarmClientPartials/CrudPartial.cs
@@ -17,17 +17,33 @@ namespace FarmMasterTests.Common
 {
     public partial class FarmClient
     {
-        public async Task<HttpResponseMessage> CrudCreateAsync<T>(string url, T entity)
+        public Task<HttpResponseMessage> CrudCreateAsync<T>(string url, T entity)
+        where T : class
+        {
+            return this.CrudCreateEditAsync(url, entity, true);
+        }
+
+        public Task<HttpResponseMessage> CrudEditAsync<T>(string url, T entity)
+        where T : class
+        {
+            return this.CrudCreateEditAsync(url, entity, false);
+        }
+
+        private async Task<HttpResponseMessage> CrudCreateEditAsync<T>(string url, T entity, bool isCreate)
         where T : class
         {
             var model = new CrudCreateEditViewModel<T> 
             {
-                IsCreate = true,
+                IsCreate = isCreate,
                 Entity   = entity
             };
 
-            // Test for redirect, since it should be going to an edit page, instead of erroring to the create page.
-            return await this.PostEnsureStatusAsync(url, model.ToFormEncodedContent(), HttpStatusCode.Redirect);
+            
+            var codeToCheck = (isCreate) 
+                ? HttpStatusCode.Redirect // Test for redirect, since it should be going to an edit page, instead of erroring to the create page.
+                : HttpStatusCode.OK;      // Test for going back to the same page.
+
+            return await this.PostEnsureStatusAsync(url, model.ToFormEncodedContent(), codeToCheck);
         }
     }
 }
--- FarmMasterTests/Integration/CrudTests.cs
+++ FarmMasterTests/Integration/CrudTests.cs
@@ -1,7 +1,9 @@
-﻿using FarmMasterTests.Common;
+﻿using DataAccess;
+using FarmMasterTests.Common;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -14,10 +16,47 @@ namespace FarmMasterTests.Integration
         }
 
         [Fact()]
-        public async void ContactCreateTest()
+        public async void ContactTest()
         {
+            // Create
             await this.Client.LoginAsync();
+            await this.Client.CrudCreateAsync("/Contact/Create", new Contact
+            { 
+                Name  = "Bradley Chatha",
+                Email = "bradley.chatha@gmail.com",
+                Phone = "69420",
+                Type  = ContactType.Individual
+            });
 
+            // Read(Index)
+            var response = await this.Client.GetEnsureStatusAsync("/Contact", HttpStatusCode.OK);
+            var content  = await response.Content.ReadAsStringAsync();
+            Assert.Contains("Bradley Chatha", content);
+
+            // Read(Edit page)
+            response = await this.Client.GetEnsureStatusAsync("/Contact/Edit?id=1", HttpStatusCode.OK);
+            content  = await response.Content.ReadAsStringAsync();
+            Assert.Contains("Bradley Chatha",               content);
+            Assert.Contains("bradley.chatha@gmail.com",     content);
+            Assert.Contains("69420",                        content);
+            Assert.Contains(nameof(ContactType.Individual), content);
+
+            // Edit & Read to ensure all changes take affect. (code dupe, but meh, I've had situtations before where singular fields won't update, so I want to test them all)
+            await this.Client.CrudEditAsync("/Contact/Edit", new Contact
+            {
+                ContactId = 1,
+                Email     = "sealabjaster@gmail.com",
+                Name      = "Brandy Chaser",
+                Phone     = "666",
+                Type      = ContactType.BusinessEntity
+            });
+
+            response = await this.Client.GetEnsureStatusAsync("/Contact/Edit?id=1", HttpStatusCode.OK);
+            content  = await response.Content.ReadAsStringAsync();
+            Assert.Contains("Brandy Chaser",                    content);
+            Assert.Contains("sealabjaster@gmail.com",           content);
+            Assert.Contains("666",                              content);
+            Assert.Contains(nameof(ContactType.BusinessEntity), content);
         }
     }
 }
